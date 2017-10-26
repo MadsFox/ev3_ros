137e264b7b6b685816d14de3645f56d698bb4847
madssk <mads.saustrup.kristensen@gmail.com>
2017-10-26 11:18:30 +0200

    added keyboard controller to motor_command_pub

---
+++ src/motor_command_pub_keyboard.cpp
@@ -0,0 +1,42 @@
+#include "ros/ros.h"
+#include "ev3_ros/MotorCommands.h"
+
+using ev3_ros::MotorCommands;
+//using namespace ros;
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "motor_command_pub_test_node");
+
+  ros::NodeHandle n;
+
+  ros::Publisher motor_command_pub = n.advertise<MotorCommands>("motor_command", 1000);
+
+  ros::Rate loop_rate(10);
+
+  int duration = 0;
+
+  MotorCommands mc;
+
+  while (ros::ok())
+  {
+    switch(duration%100) {
+      case 1 :
+        mc.speed = 100;
+        break;
+      case 6 :
+        mc.speed = -100;
+        break;
+    }
+  
+    motor_command_pub.publish(mc);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+
+    ++duration;
+  }
+
+  return 0;
+}
