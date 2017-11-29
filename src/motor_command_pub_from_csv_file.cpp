88549212b07c24937fc5671f59eff7b7fe64ec01
madssk <mads.saustrup.kristensen@gmail.com>
2017-11-29 15:40:23 +0100

    *_from_csv_file compiling, still need to remove qoute signs from values

--- src/motor_command_pub_from_csv_file.cpp
+++ src/motor_command_pub_from_csv_file.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <map>
 #include "ros/ros.h"
 #include "ev3_ros/MotorCommands.h"
 
@@ -10,9 +12,11 @@ using ev3_ros::MotorCommands;
 using namespace std;
 
 int main(int argc, char **argv) {
+    int counter;
     char str[256];
     //vector for all the data in the csv file.
     vector< vector<string> > fileData;
+map<string, ros::Publisher> robotPub;
 
     //ask in terminal for CSV file 
     cout << "Enter URI of motorCommand .csv file: ";
@@ -21,7 +25,7 @@ int main(int argc, char **argv) {
 
     //checks if file is CSV file
     if (fileName.substr(fileName.find_last_of(".") + 1) != "csv") {
-        cout << "not a csv file";
+        cout << "not a csv file" << endl;
         return 0;
     }
 
@@ -39,21 +43,40 @@ int main(int argc, char **argv) {
             string s;
             if(!getline(ss,s,',')) break;
             record.push_back(s);
-            cout << s;
+            counter++;
         }
         fileData.push_back(record);
-        cout << endl;
+       
     }
-    return 0;
-
+    cout << counter << " commands in file" << endl;
+    
     //initiate the ROS node
-    ros::init(argc, argv, "motor_command_pub_test_node");
+    ros::init(argc, argv, "motor_command_pub_from_csv_file");
 
     //new nodehandler to send commands
     ros::NodeHandle n;
+    
+    for(vector< vector<string> >::iterator it = fileData.begin(); it != fileData.end(); ++it){
+        //cout << it->at(0).c_str() << endl;
+        string startTime = it->at(0).c_str(); 
+        string name = it->at(1).c_str();
+        if(startTime.compare("startTime") == 0){
+            cout << "header line" << endl;
+        }else if (name.compare("name") != 0){
+            if(robotPub.find(name) != robotPub.end()){
+                cout << name << " is already known" << endl;
+            }else{
+                cout << "new robot by the name: " << name << endl;
+                
+                robotPub.insert(pair<string, ros::Publisher>(name, n.advertise<MotorCommands>(name, 1000)));
+            
+            }
+        }
+    }
+    
+    return 0;
 
-    ros::Publisher motor_command_pub = n.advertise<MotorCommands>("motor_command", 1000);
-
+    
     ros::Rate loop_rate(10);
 
     int duration = 0;
