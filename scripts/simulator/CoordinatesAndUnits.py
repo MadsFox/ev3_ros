93cc1c8c39ef58856764a49ab72635ab09ba2e51
madssk <mads.saustrup.kristensen@gmail.com>
2017-12-11 16:26:08 +0100

    'test' of CoordinatsAndUnits class implementet

--- scripts/simulator/CoordinatesAndUnits.py
+++ scripts/simulator/CoordinatesAndUnits.py
@@ -45,10 +45,10 @@ class ScenePoint:
         self.y = yy
 
     def klone(self):
-        return self.ScenePoint(self.x, self.y)
+        return ScenePoint(self.x, self.y)
 
     def toString(self):
-        return "<" + self.x + "," + self.y + ">"
+        return "<{},{}>".format(self.x, self.y)
 
     def runaway(self):
         return self.x > hsw or self.x < -hsw or self.y > sd or self.y < 0
@@ -62,37 +62,36 @@ class ScenePoint:
         cosNewDir = cos(radians(phi))
         self.moveRelXRelY(sinNewDir * deltaDist, cosNewDir * deltaDist)
 
+    def avg(self, point):
+        return ScenePoint((self.x + point.x) / 2, (self.y + point.y) / 2)
+
 
 # easy constructor
 def p(x, y):
     return ScenePoint(x, y)
 
 
-def dist(p1, p2):
-    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y))
-
-
-def avg(p1, p2):
-    return ScenePoint((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
-
-
 class Pose:  # for a robot
     position = ScenePoint
     direction = 0  # 0..360
 
-    def __init__(self, *args, **kwargs):
-        if "xx" and "yy" in kwargs:
-            self.position = ScenePoint(kwargs.get("x"), kwargs.get("y"))
-        elif "ss" in kwargs:
-            self.position = kwargs.get("ss")
-        self.direction = kwargs.get("dd")
+    def __init__(self, xx, yy, dd=None):
+        if dd is None:
+            self.position = xx
+            self.direction = yy
+        else:
+            self.position = ScenePoint(xx, yy)
+            self.direction = dd
 
     def klone(self):
         return Pose(self.position.klone(),
                     self.direction)  # intuitively same as clone, but returns object of right class
 
     def toString(self):
-        return self.position + "*phi=" + self.direction
+        return "<{},{}*phi={}>".format(self.position.x, self.position.y, self.direction)
+
+    def runaway(self):
+        return self.position.runaway()
 
     def moveRelPhiD(self, deltaPhi, deltaDist):
         # phi added to current direction, but scaled by deltaDist AND SOME CONSTANT THAT DEPENDS ON PHYSICAL DETAILS OF THE ROBOT
@@ -103,18 +102,16 @@ class Pose:  # for a robot
         self.direction = normalizeAngle(phi)
         self.position.moveRelPhiD(self.direction, deltaDist)
 
+    def avg(self, pose):
+        return Pose(self.position.avg(pose.position), (self.direction + pose.direction) / 2)
+
 
 # easy constructor:
-def pose(x, y, d):
-    return Pose(xx=x, yy=y, dd=d)
-
-
-def pose(s, d):
-    return Pose(ss=s, dd=d)
-
-
-def avg(p1, p2):
-    return Pose(ss=avg(p1.position, p2.position), dd=(p1.direction + p2.direction) / 2)
+def pose(x, y, d=None):
+    if d is None:
+        return Pose(x, y)
+    else:
+        return Pose(x, y, d)
 
 
 # directions internally in degrees memotech. as n,e,s,w etc.
@@ -163,14 +160,14 @@ robotTurningDiameter = 0.7
 # in mkXXXXXRouteXXXX should be correlated with constant epsilon and expected robot diameter
 
 def dist(p1, p2):
-    # plain euclidian, ignoring direction
-    return dist(p1.position, p2.position)
-
-
-def dist(p1, p2): return dist(p1.position, p2)
-
-
-def dist(p1, p2): return dist(p1, p2.position)
+    if isinstance(p1, Pose) and isinstance(p2, Pose):
+        p1 = p1.position
+        p2 = p2.position
+    elif isinstance(p1, ScenePoint) and isinstance(p2, Pose):
+        p2 = p2.position
+    elif isinstance(p1, Pose) and isinstance(p2, ScenePoint):
+        p1 = p1.position
+    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y))
 
 
 if __name__ == '__main__':
@@ -178,34 +175,68 @@ if __name__ == '__main__':
     setWidth(1000)
     setSceneDepth(50)
     setSceneWidth(100)
-    print("testing setHeight(2000): " + height
-          + " setWidth(1000): " + width
-          + " setSceneDepth(5): " + sd
-          + " setSceneWidth(10)" + sw
-          + " half SceneWidth: " + hsw)
+    print("testing setHeight(2000): {}".format(height))
+    print("setWidth(1000):          {}".format(width))
+    print("setSceneDepth(5):        {}".format(sd))
+    print("setSceneWidth(100):      {}".format(sw))
+    print("half SceneWidth:         {}".format(hsw))
 
+    print("")
     print("Testing ScenePoint")
 
     s = ScenePoint(10, 20)
-    s2 = s.clone()
-    p = p(20, 30)
-    print("ScenePoint(10, 20): " + s.toString()
-          + "s.clone(): " + s2.toString()
-          + "p(20, 30): " + p.toString())
-    
-    print("s.runaway()" + s.runaway())
+    s2 = s.klone()
+    s3 = p(10, 30)
+    print("s = ScenePoint(10, 20):  {}".format(s.toString()))
+    print("s2 = s.clone():          {}".format(s2.toString()))
+    print("s3 = p(20, 30):          {}".format(s3.toString()))
+
+    print("s.runaway():             {}".format(s.runaway()))
 
     s.moveRelPhiD(90, sw)
-    print("s.moveRelPhiD(90, sw)" + s.toString())
+    print("s.moveRelPhiD(90, sw):   {}".format(s.toString()))
 
-    print("s.runaway()" + s.runaway())
+    print("s.runaway():             {}".format(s.runaway()))
 
-    s.moveRelPhiD(-90, -sw)
-    print("s.moveRelPhiD(-90, -sw)" + s.toString())
+    s.moveRelPhiD(90, -sw)
+    print("s.moveRelPhiD(90, -sw):  {}".format(s.toString()))
 
     s.moveRelXRelY(sw, 0)
-    print("s.moveRelXRelY(sw, 0)" + s.toString())
+    print("s.moveRelXRelY(sw, 0):   {}".format(s.toString()))
+    s.moveRelXRelY(-sw, 0)
+    print("s.moveRelXRelY(-sw, 0):  {}".format(s.toString()))
 
+    print("dist(s, s3):              {}".format(dist(s, s3)))
 
+    print("s.avg(s3):                {}".format(s.avg(s3).toString()))
+
+    print("")
+    print("Testing ScenePoint")
+
+    p = Pose(10, 20, 90)
+    pk = s.klone()
+    p3 = pose(10, 30, 0)
+    print("p = Pose(10, 20):        {}".format(p.toString()))
+    print("pk = s.clone():          {}".format(pk.toString()))
+    print("p3 = pose(20, 30, 0):    {}".format(p3.toString()))
+
+    print("p.runaway():             {}".format(p.runaway()))
+
+    p.moveRelPhiD(0, sw)
+    print("p.moveRelPhiD(90, sw):   {}".format(p.toString()))
+
+    print("p.runaway():             {}".format(p.runaway()))
+
+    p.moveRelPhiD(90, -sw)
+    print("p.moveRelPhiD(90, -sw):  {}".format(p.toString()))
+
+    p.moveAbsPhiD(90, 0)
+    print("p.moveRelXRelY(sw, 0):   {}".format(p.toString()))
+    p.moveAbsPhiD(-90, 0)
+    print("p.moveRelXRelY(-sw, 0):  {}".format(p.toString()))
+
+    print("dist(p, p3):              {}".format(dist(p, p3)))
+
+    print("p.avg(p3):                {}".format(p.avg(p3).toString()))
 
 
